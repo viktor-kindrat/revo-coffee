007e787facc4c88bcc8430eee97cde27673df871
Victor Kindrat <kinrdatvictor07@gmail.com>
2022-10-01 18:25:16 +0300

    upd

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let host = '127.0.0.1';
 let port = 8000;
 
 let express = require('express');
-let favicon = require('serve-favicon');
+// let favicon = require('serve-favicon');
 let path = require('path');
 // const open = require("open");
 
@@ -10,7 +10,7 @@ let path = require('path');
 let app = express();
 
 app.use(express.static(__dirname));
-app.use(favicon(path.join(__dirname, 'image', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'image', 'favicon.ico')))
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html')
