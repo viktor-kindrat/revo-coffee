0fb4d8922e7e10da35b6e5a9a3a1ac9c25249429
Victor Kindrat <kinrdatvictor07@gmail.com>
2022-10-01 18:15:46 +0300

    update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let port = 8000;
 let express = require('express');
 let favicon = require('serve-favicon');
 let path = require('path');
-const open = require("open");
+// const open = require("open");
 
 
 let app = express();
@@ -19,4 +19,4 @@ app.get('/', function(request, response) {
 app.listen(port, host);
 
 console.log(`Your app listening on: http://${host}:${port}`)
-open(`http://${host}:${port}`);
+// open(`http://${host}:${port}`);
