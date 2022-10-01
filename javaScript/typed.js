2c4a845e55de8fb14026295a2ac81db4aa6c090d
Victor Kindrat <kinrdatvictor07@gmail.com>
2022-10-01 17:33:02 +0300

    prepare first screen

---
+++ javaScript/typed.js
@@ -0,0 +1,11 @@
+let options = {
+  strings: ['BEST', 'PERSONAILIZED'],
+  typeSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: false,
+  backSpeed: 40,
+  backDelay: 5000,
+};
+
+let typed = new Typed('.home__typer', options);
