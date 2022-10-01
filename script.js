efcc4d4cd9cd8042f725f13c51bd02592a98a4b3
Victor Kindrat <kinrdatvictor07@gmail.com>
2022-10-01 18:00:44 +0300

    upd

---
+++ script.js
@@ -0,0 +1,22 @@
+let host = '127.0.0.1';
+let port = 8000;
+
+let express = require('express');
+let favicon = require('serve-favicon');
+let path = require('path');
+const open = require("open");
+
+
+let app = express();
+
+app.use(express.static(__dirname));
+app.use(favicon(path.join(__dirname, 'image', 'favicon.ico')))
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, host);
+
+console.log(`Your app listening on: http://${host}:${port}`)
+open(`http://${host}:${port}`);
